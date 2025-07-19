fae9dab101f5847803b9708ab90d6c2df58928b3
Gerd <nathan.j.s@hotmail.com>
2025-07-18 19:52:44 -0700

    More Nuking - Stripped down mainwindow.xaml.cs

--- MonitorManagerCS-GUI/MainWindow.xaml.cs
+++ MonitorManagerCS-GUI/MainWindow.xaml.cs
@@ -20,7 +20,7 @@ namespace MonitorManagerCS_GUI
         private ContextMenuStrip trayMenuStrip;
         private ToolStripMenuItem showTrayMenuItem;
         private ToolStripMenuItem exitTrayMenuItem;
-        public MainViewModel ViewModel { get; set; }
+        public MainViewModel mainViewModel { get; set; }
         public SettingsGridItem settingDimStart;
         public SettingsGridItem settingDimEnd;
         public SettingsGridItem settingBrightStart;
@@ -53,57 +53,12 @@ namespace MonitorManagerCS_GUI
             //Run the MainWindow_StateChanged function when the window's state changes (e.g. when the window is opened, minimized, or closed)
             StateChanged += MainWindow_StateChanged;
 
-            MinimizeToTray();
+            //MinimizeToTray();
 
-            ViewModel = new MainViewModel();
-            DataContext = ViewModel;
-
-            //Load settings
-            startupSettings = App.InitStartupSettings();
-            settings = App.InitSettings(startupSettings);
-
-            //Update the text in the settings file path text box
-            TxtSettingsPath.Text = startupSettings.SettingsFilePath;
-
-            StartMonitorService();
-
-            PopulateSettingsGrid(settings);
+            mainViewModel = new MainViewModel();
+            DataContext = mainViewModel;
         }
 
-        public void PopulateSettingsGrid(Settings settings)
-        {
-            //Throw an exception if the settings don't exist
-            if (settings == null) { throw new ArgumentNullException(nameof(settings), "Settings cannot be null"); }
-
-            //Clear the settings data grid
-            ViewModel.SettingsGridData.Clear();
-
-            //Add all the settings to the settings data grid
-            settingDimStart = new SettingsGridItem("When to start decreasing brightness", DataFormatter.GetReadableTime(DefaultSettings.DimStartHour, DefaultSettings.DimStartMinute), DataFormatter.GetReadableTime(settings.DimStartHour, settings.DimStartMinute));
-            ViewModel.SettingsGridData.Add(settingDimStart);
-            settingDimEnd = new SettingsGridItem("When monitors should reach minimum brightness", DataFormatter.GetReadableTime(DefaultSettings.DimEndHour, DefaultSettings.DimEndMinute), DataFormatter.GetReadableTime(settings.DimEndHour, settings.DimEndMinute));
-            ViewModel.SettingsGridData.Add(settingDimEnd);
-            settingBrightStart = new SettingsGridItem("When to start increasing brightness", DataFormatter.GetReadableTime(DefaultSettings.BrightStartHour, DefaultSettings.BrightStartMinute), DataFormatter.GetReadableTime(settings.BrightStartHour, settings.BrightStartMinute));
-            ViewModel.SettingsGridData.Add(settingBrightStart);
-            settingBrightEnd = new SettingsGridItem("When monitors should reach maximum brightness", DataFormatter.GetReadableTime(DefaultSettings.BrightEndHour, DefaultSettings.BrightEndMinute), DataFormatter.GetReadableTime(settings.BrightEndHour, settings.BrightEndMinute));
-            ViewModel.SettingsGridData.Add(settingBrightEnd);
-            settingMonitorLeft = new SettingsGridItem("Left monitor name or ID", DefaultSettings.MonitorLeft, settings.MonitorLeft);
-            ViewModel.SettingsGridData.Add(settingMonitorLeft);
-            settingMonitorCenter = new SettingsGridItem("Center monitor name or ID", DefaultSettings.MonitorCenter, settings.MonitorCenter);
-            ViewModel.SettingsGridData.Add(settingMonitorCenter);
-            settingMonitorRight = new SettingsGridItem("Right monitor name or ID", DefaultSettings.MonitorRight, settings.MonitorRight);
-            ViewModel.SettingsGridData.Add(settingMonitorRight);
-            settingLeftBrightness = new SettingsGridItem("Left monitor brightness range", DataFormatter.GetMinMaxString(DefaultSettings.LeftMinBrightness, DefaultSettings.LeftMaxBrightness), DataFormatter.GetMinMaxString(settings.LeftMinBrightness, settings.LeftMaxBrightness));
-            ViewModel.SettingsGridData.Add(settingLeftBrightness);
-            settingCenterBrightness = new SettingsGridItem("Center monitor brightness range", DataFormatter.GetMinMaxString(DefaultSettings.CenterMinBrightness, DefaultSettings.CenterMaxBrightness), DataFormatter.GetMinMaxString(settings.CenterMinBrightness, settings.CenterMaxBrightness));
-            ViewModel.SettingsGridData.Add(settingCenterBrightness);
-            settingRightBrightness = new SettingsGridItem("Right monitor brightness range", DataFormatter.GetMinMaxString(DefaultSettings.RightMinBrightness, DefaultSettings.RightMaxBrightness), DataFormatter.GetMinMaxString(settings.RightMinBrightness, settings.RightMaxBrightness));
-            ViewModel.SettingsGridData.Add(settingRightBrightness);
-            settingCenterBlueFilter = new SettingsGridItem("Center monitor blue light filter range", DataFormatter.GetMinMaxString(DefaultSettings.MinBlueLightFilter, DefaultSettings.MaxBlueLightFilter), DataFormatter.GetMinMaxString(settings.MinBlueLightFilter, settings.MaxBlueLightFilter));
-            ViewModel.SettingsGridData.Add(settingCenterBlueFilter);
-            settingBrightCheckTime = new SettingsGridItem("Time between brightness updates (seconds)", DefaultSettings.BrightCheckTime.ToString(), DefaultSettings.BrightCheckTime.ToString());
-            ViewModel.SettingsGridData.Add(settingBrightCheckTime);
-        }
 
         private void StartMonitorService()
         {
@@ -272,15 +227,7 @@ namespace MonitorManagerCS_GUI
             trayIcon.Dispose();
         }
 
-        private void BtnSettingsPath_Click(object sender, RoutedEventArgs e)
-        {
-            string filePath = GetFilePath();
-            if (filePath == null) { return; }
-
-            TxtSettingsPath.Text = filePath;
-        }
-
-        public string GetFilePath()
+        public string GetSettingsPathFromUser()
         {
             OpenFileDialog fileDialog = new OpenFileDialog
             {
@@ -295,7 +242,7 @@ namespace MonitorManagerCS_GUI
                 if (fileDialog.FileName == StartupSettings.StartupSettingsFilePath)
                 {
                     e.Cancel = true;
-                    System.Windows.MessageBox.Show($"The file path 'StartupSettings.StartupSettingsFilePath' is reserved for startup settings.", "Invalid Filename", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    System.Windows.MessageBox.Show($"The file path '{StartupSettings.StartupSettingsFilePath}' is reserved for startup settings.", "Invalid Filename", MessageBoxButton.OK, MessageBoxImage.Warning);
                 }
             };
 
@@ -314,142 +261,6 @@ namespace MonitorManagerCS_GUI
             string output = statusPrefixes[statusPrefixIndex++];
             if (statusPrefixIndex >= statusPrefixes.Length) statusPrefixIndex = 0;
             return output;
-        }
-
-        private void BtnUpdateSettings_Click(object sender, RoutedEventArgs e)
-        {
-            //When you press the update settings button
-            //Take each "current" setting from the grid list, put it into the settings object, and then write it to the settings file.
-            bool invalidSettings = false;
-            string invalidList = "";
-
-            void InvalidSetting(string invalidText, string inputString) { invalidSettings = true; invalidList += $"{invalidText}: {inputString}\n"; }
-
-            void CheckSettingRegex(Regex regex, string inputString, string invalidText)
-            {
-                if (!regex.IsMatch(inputString)) { InvalidSetting(invalidText, inputString); }
-            }
-
-            //Check if the inputted settings are valid
-            CheckSettingRegex(DataInterpreter.readableTimeRegex, settingDimStart.CurrentVal, "Invalid time");
-            CheckSettingRegex(DataInterpreter.readableTimeRegex, settingDimEnd.CurrentVal, "Invalid time");
-            CheckSettingRegex(DataInterpreter.readableTimeRegex, settingBrightStart.CurrentVal, "Invalid time");
-            CheckSettingRegex(DataInterpreter.readableTimeRegex, settingBrightEnd.CurrentVal, "Invalid time");
-            CheckSettingRegex(DataInterpreter.minMaxRegex, settingLeftBrightness.CurrentVal, "Invalid brightness range");
-            CheckSettingRegex(DataInterpreter.minMaxRegex, settingCenterBrightness.CurrentVal, "Invalid brightness range");
-            CheckSettingRegex(DataInterpreter.minMaxRegex, settingRightBrightness.CurrentVal, "Invalid brightness range");
-            CheckSettingRegex(DataInterpreter.blueLightRegex, settingCenterBlueFilter.CurrentVal, "Invalid blue light filter range");
-
-            if (!int.TryParse(settingBrightCheckTime.CurrentVal, out int brightCheckTime))
-            {
-                InvalidSetting("Invalid time between brightness updates", settingBrightCheckTime.CurrentVal);
-            }
-
-            //If any of the settings are invalid, show an error message and exit early
-            if (invalidSettings)
-            {
-                //If the settings are invalid, show an error message to the user
-                System.Windows.MessageBox.Show($"{invalidList}", "Invalid Settings", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-            //If the settings are valid...
-
-            //Convert the time into the hour and minute bytes and store them in the settings
-            byte[] twoBytes = DataInterpreter.ParseReadableTime(settingDimStart.CurrentVal);
-            settings.DimStartHour = twoBytes[0];
-            settings.DimStartMinute = twoBytes[1];
-
-            twoBytes = DataInterpreter.ParseReadableTime(settingDimEnd.CurrentVal);
-            settings.DimEndHour = twoBytes[0];
-            settings.DimEndMinute = twoBytes[1];
-
-            twoBytes = DataInterpreter.ParseReadableTime(settingBrightStart.CurrentVal);
-            settings.BrightStartHour = twoBytes[0];
-            settings.BrightStartMinute = twoBytes[1];
-
-            twoBytes = DataInterpreter.ParseReadableTime(settingBrightEnd.CurrentVal);
-            settings.BrightEndHour = twoBytes[0];
-            settings.BrightEndMinute = twoBytes[1];
-
-            //Store monitor ids in settings
-            settings.MonitorLeft = settingMonitorLeft.CurrentVal;
-            settings.MonitorCenter = settingMonitorCenter.CurrentVal;
-            settings.MonitorRight = settingMonitorRight.CurrentVal;
-
-            //Store min and max brightnesses in the settings
-            twoBytes = DataInterpreter.ParseMinMaxString(settingLeftBrightness.CurrentVal);
-            settings.LeftMinBrightness = twoBytes[0];
-            settings.LeftMaxBrightness = twoBytes[1];
-
-            twoBytes = DataInterpreter.ParseMinMaxString(settingCenterBrightness.CurrentVal);
-            settings.CenterMinBrightness = twoBytes[0];
-            settings.CenterMaxBrightness = twoBytes[1];
-
-            twoBytes = DataInterpreter.ParseMinMaxString(settingRightBrightness.CurrentVal);
-            settings.RightMinBrightness = twoBytes[0];
-            settings.RightMaxBrightness = twoBytes[1];
-
-            //Store blue light filter range in the settings
-            twoBytes = DataInterpreter.ParseMinMaxString(settingCenterBlueFilter.CurrentVal);
-            settings.MinBlueLightFilter = twoBytes[0];
-            settings.MaxBlueLightFilter = twoBytes[1];
-
-            //Store the brightness check time in the settings
-            settings.BrightCheckTime = brightCheckTime;
-
-            //Save the settings file
-            settings.WriteSettingsFile(startupSettings.SettingsFilePath);
-
-            //Show a status message to let the user know it worked
-            TxtStatus.Text = $"{StatusPrefix()} Settings saved to {startupSettings.SettingsFilePath}";
-            //Restart the monitor manager service
-            StartMonitorService();
-        }
-
-        private void BtnLoadSettings_Click(object sender, RoutedEventArgs e)
-        {
-            //If the settings path doesn't lead to a file, show an error message
-            if (!File.Exists(TxtSettingsPath.Text))
-            {
-                System.Windows.MessageBox.Show($"File does not exist: {TxtSettingsPath.Text}", "Invalid file path",MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            //Ask the user if they want to continue
-            MessageBoxResult msgBoxResult = System.Windows.MessageBox.Show($"Are you sure you want to change the settings file path to {TxtSettingsPath.Text}?", "Change settings file path?", MessageBoxButton.YesNo);
-            if (msgBoxResult == MessageBoxResult.No)
-            {
-                return;
-            }
-
-            //Read the text out of the file and store it in a string
-            string settingsText = File.ReadAllText(TxtSettingsPath.Text);
-
-            //Attempt to read the settings file
-            try
-            {
-                JsonSerializer.Deserialize<Settings>(settingsText);
-            }
-            //If the json reader can't read the settings file, show an error message
-            catch (Exception)
-            {
-                System.Windows.MessageBox.Show($"Failed to read file\n {TxtSettingsPath.Text}", "Invalid file format", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-
-            //Read the settings from the settings path
-            settings.ReadSettingsFile(TxtSettingsPath.Text);
-            //Update the data grid with the new settings
-            PopulateSettingsGrid(settings);
-            //Update the startup settings to the new path
-            startupSettings.SettingsFilePath = TxtSettingsPath.Text;
-            //Update the startup settings file
-            startupSettings.WriteStartupSettingsFile();
-
-            //Show a status message to let the user know it worked
-            TxtStatus.Text = $"{StatusPrefix()}Loaded settings from {TxtSettingsPath.Text}";
-            //Restart the monitor service to use the new settings
-            StartMonitorService();
         }
 
         protected override void OnClosing(CancelEventArgs e)
