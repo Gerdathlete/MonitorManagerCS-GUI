8c168684814a13b93c078d6af4568f1a10b08604
Gerd <nathan.j.s@hotmail.com>
2025-08-03 21:17:11 -0700

    Removed outdated RunProgram method

--- MonitorManagerCS-GUI/Core/Programs.cs
+++ MonitorManagerCS-GUI/Core/Programs.cs
@@ -10,31 +10,6 @@ namespace MonitorManagerCS_GUI.Core
         internal static readonly string ControlMyMonitor = 
             Path.Combine(Folders.ControlMyMonitor, "ControlMyMonitor.exe");
 
-        /// <summary>
-        /// Runs the given program and returns the text output
-        /// </summary>
-        /// <param name="program"></param>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        internal static string RunProgram(string program, string args)
-        {
-            using (var process = new Process())
-            {
-                process.StartInfo.FileName = program;
-                process.StartInfo.Arguments = args;
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.RedirectStandardOutput = true;
-
-                process.Start();
-
-                string textOutput = process.StandardOutput.ReadToEnd();
-
-                process.WaitForExit();
-
-                return textOutput;
-            }
-        }
-
         internal static async Task<string> RunProgramAsync(string program, string args)
         {
             var tcs = new TaskCompletionSource<string>();
